1e4e6353b496c79d3a3dc33cfdbd06d56a2764d5
zhanzhihu <noodzhan@163.com>
2023-07-31 16:28:49 +0800

    fix: 优化代码逻辑

--- src/watchdog/nacos_watchdog.sh
+++ src/watchdog/nacos_watchdog.sh
@@ -1,26 +1,40 @@
 #!/bin/bash
 set -e
-nacos_alive=$(netstat -tunlp | grep 8848 | wc -l)
-install_dir=/usr/local
-check_url=http://localhost:8726/check
-nacos_user=nacos
-nacos_passwd=nacos@haici5
-function start_nacos() {
-         sudo su <<EOF
-         source /etc/profile
-         echo "JAVA_HOME: ${JAVA_HOME}"
-         ${install_dir}/nacos/bin/startup.sh -m standalone
-EOF
+
+# 配置信息
+install_dir="/usr/local"
+check_url="http://localhost:8726/check"
+nacos_user="nacos"
+nacos_passwd="nacos@haici5"
+
+# 检查端口是否处于监听状态
+function check_port() {
+    local port=$1
+    netstat -tunlp | grep "$port" | wc -l
 }
 
-if [ $nacos_alive -eq 0 ]; then
-  # 启动
-  start_nacos
-else
-  # 远程调用
-  res=$(curl -X POST '127.0.0.1:8848/nacos/v1/auth/login' -d "username=${nacos_user}&password=${nacos_passwd}")
-  sleep 5
-  if [ $(echo $res | grep accessToken | wc -l) -ne 1 ]; then
+# 启动Nacos服务
+function start_nacos() {
+    source /etc/profile
+    echo "JAVA_HOME: ${JAVA_HOME}"
+    "${install_dir}/nacos/bin/startup.sh" -m standalone
+}
+
+# 远程调用认证接口
+function remote_auth() {
+    local username=$1
+    local password=$2
+    curl -X POST '127.0.0.1:8848/nacos/v1/auth/login' -d "username=${username}&password=${password}"
+}
+
+# 检查端口是否已监听，未监听则启动Nacos服务
+if [[ $(check_port 8848) -eq 0 ]]; then
     start_nacos
-  fi
+else
+    # 远程调用认证接口
+    res=$(remote_auth "$nacos_user" "$nacos_passwd")
+    sleep 5
+    if [[ $(echo "$res" | grep -c "accessToken") -ne 1 ]]; then
+        start_nacos
+    fi
 fi
