594cce664e56e9ea48af8e84f7d63ae584e57862
zhanzhihu <noodzhan@163.com>
2023-07-31 16:22:06 +0800

    feat: 验证nacos是否正常

--- src/watchdog/nacos_watchdog.sh
+++ src/watchdog/nacos_watchdog.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-
-nacos_alive=${netstat -tunlp | grep 8848|wc -l}
+set -e
+nacos_alive=$(netstat -tunlp | grep 8848 | wc -l)
 install_dir=/usr/local
 check_url=http://localhost:8726/check
+nacos_user=nacos
+nacos_passwd=nacos@haici5
 function start_nacos() {
-    	sudo su <<EOF
-    	source /etc/profile
-    	echo "JAVA_HOME: ${JAVA_HOME}"
-    	${install_dir}/nacos/bin/startup.sh -m standalone
+         sudo su <<EOF
+         source /etc/profile
+         echo "JAVA_HOME: ${JAVA_HOME}"
+         ${install_dir}/nacos/bin/startup.sh -m standalone
 EOF
 }
 
@@ -16,8 +18,9 @@ if [ $nacos_alive -eq 0 ]; then
   start_nacos
 else
   # 远程调用
-  res=$(curl $check_url)
-  if [res -ne 0]
+  res=$(curl -X POST '127.0.0.1:8848/nacos/v1/auth/login' -d "username=${nacos_user}&password=${nacos_passwd}")
+  sleep 5
+  if [ $(echo $res | grep accessToken | wc -l) -ne 1 ]; then
     start_nacos
   fi
 fi
