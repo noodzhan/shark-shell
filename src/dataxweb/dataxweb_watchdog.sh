4f2365c02df3993136aff61c8586725b848cabc1
zhanzhihu <noodzhan@163.com>
2023-08-25 14:05:00 +0800

    fix: 添加shell

---
+++ src/dataxweb/dataxweb_watchdog.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+datax_admin_install="/usr/local/datax-admin"
+datax_executor_install="/usr/local/datax-executor"
+jps="/usr/local/java/bin/jps"
+
+function start_datax_admin() {
+    "${datax_admin_install}/bin/datax-admin.sh" start
+}
+
+function start_datax_executor() {
+    "${datax_executor_install}/bin/datax-executor.sh" start
+}
+
+
+
+jps_result=$("${jps}")
+
+datax_admin_count=$(echo $jps_result | grep "DataXAdminApplication" | wc -l)
+
+if [ "${datax_admin_count}" -eq 0 ]; then
+    start_datax_admin
+fi
+
+datax_executor_count=$(echo $jps_result | grep "DataXExecutorApplication" | wc -l)
+
+if [ "${datax_executor_count}" -eq 0 ]; then
+    start_datax_executor
+fi
+
+
+
+
