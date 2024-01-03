86b75043e5881b6cac32d2b2ee25fdd80338a334
noodzhan <noodzhan@163.com>
2024-01-03 08:23:56 +0800

    feat: 优化check frps逻辑

--- src/frps/check_frps.sh
+++ src/frps/check_frps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-resp=$(curl 'http://1.15.231.74:7500/api/proxy/tcp' \
+resp=$(curl -s 'http://1.15.231.74:7500/api/proxy/tcp' \
   -H 'Accept: */*' \
   -H 'Accept-Language: zh-CN,zh;q=0.9' \
   -H 'Authorization: Basic YWRtaW46bm9vZHpoYW5AMTIzNA==' \
@@ -10,9 +10,11 @@ resp=$(curl 'http://1.15.231.74:7500/api/proxy/tcp' \
   --compressed \
   --insecure --connect-timeout 30)
 
-# echo $resp
-
-if [[ $(echo $resp | grep "offline" | wc -l) -gt 0 ]]; then
+## sudo apt install jq
+ssh_status=$(echo $resp | jq -r '.proxies[] | select(.name == "ssh") | .status')
+jeecgboot_status=$(echo $resp | jq -r '.proxies[] | select(.name == "jeecgboot") | .status')
+mysql_status=$(echo $resp | jq -r '.proxies[] | select(.name == "mysql") | .status')
+if [[ $ssh_status == "offline" || $jeecgboot_status == "offline" || $mysql_status == "offline" ]]; then
   ## sudo apt install mailutils
-  echo $resp | iconv -t utf-8 | mail -s "frp-alter" noodzhan@163.com
+  echo $resp | jq '.' | iconv -t utf-8 | mail -s "frp-alter" noodzhan@163.com
 fi
