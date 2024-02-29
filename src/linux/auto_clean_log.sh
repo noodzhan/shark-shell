be622f8abdcda8b67f072faa309fe2db9491a0b6
noodzhan <noodzhan@163.com>
2024-02-29 11:32:51 +0800

    fix: 修改日志问题

--- src/linux/auto_clean_log.sh
+++ src/linux/auto_clean_log.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # 全局配置变量
-threshold_days=5       # 删除天数阈值
-size_threshold_gb=1000 # 目录大小阈值（MB）
+threshold_days=5                                # 删除天数阈值
+size_threshold_gb=1                          # 目录大小阈值（GB）
+size_threshold_mb=$((size_threshold_gb * 1024)) # 将GB转换为MB
 
 # 指定要检测和删除文件的目录列表
-dirs=("/home/yapai/test_log" "/home/yapai/test_log2")
+dirs=("/home/yapai/test_log" "/home/yapai/test_log1")
 
 # 遍历每个目录
 for dir in "${dirs[@]}"; do
@@ -17,7 +18,7 @@ for dir in "${dirs[@]}"; do
 
   # 检查目录大小是否大于阈值
   if [ "$dir_size_mb" -gt "$size_threshold_mb" ]; then
-    echo "Directory $dir size is greater than $size_threshold_gb MB ($dir_size_mb MB)"
+    echo "Directory $dir size is greater than $size_threshold_gb GB ($dir_size_mb MB)"
 
     # 计算删除日期
     delete_date=$(date -d "$threshold_days days ago" "+%Y%m%d")
@@ -31,6 +32,6 @@ for dir in "${dirs[@]}"; do
       fi
     done
   else
-    echo "Directory $dir size is not greater than $size_threshold_gb MB ($dir_size_mb MB)"
+    echo "Directory $dir size is not greater than $size_threshold_gb GB ($dir_size_mb MB)"
   fi
 done
